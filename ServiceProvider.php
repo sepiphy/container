38fa45ea8005ba88947933fdbdf9b10ddab89076
Quynh Xuan Nguyen <seriquynh@gmail.com>
2019-04-06 18:59:43 +0700

    Change again

---
+++ ServiceProvider.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of the Sepiphy package.
+ *
+ * (c) Quynh Xuan Nguyen <seriquynh@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sepiphy\PHPTools\Container;
+
+abstract class ServiceProvider
+{
+    /**
+     * @var Container
+     */
+    protected $container;
+
+    /**
+     * @param Container $container
+     */
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
+
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        //
+    }
+
+    public function when(): array
+    {
+        return [
+            //
+        ];
+    }
+}
