f00fcb152c273cae405138e1a8e74d3ee2c5f6d7
Quynh Xuan Nguyen <seriquynh@gmail.com>
2019-07-21 11:32:54 +0700

    Change sepiphy/container

--- Container.php
+++ Container.php
@@ -11,94 +11,23 @@
 
 namespace Sepiphy\PHPTools\Container;
 
-use Closure;
 use Sepiphy\PHPTools\Contracts\Container\ContainerInterface;
-use Sepiphy\PHPTools\Container\Exceptions\NotFoundException;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
-class Container implements ContainerInterface
+/**
+ * @author Quynh Xuan Nguyen <seriquynh@gmail.com>
+ */
+class Container extends ContainerBuilder implements ContainerInterface
 {
     /**
-     * @var array
-     */
-    protected $bindings = [];
-
-    /**
-     * @var array
-     */
-    protected $instances = [];
-
-    /**
      * {@inheritdoc}
      */
-    public function has($id)
+    public function useFile(string $filePath): void
     {
-        return array_key_exists($id, $this->instances) ||
-            array_key_exists($id, $this->bindings);
-    }
+        if (is_file($filePath)) {
+            $container = $this;
 
-    /**
-     * {@inheritdoc}
-     */
-    public function get($id)
-    {
-        if ($this->has($id)) {
-            return $this->make($id);
+            require $filePath;
         }
-
-        throw new NotFoundException(
-            sprintf('"%s" was not registered in the container.', $id)
-        );
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function bind(string $id, Closure $callback)
-    {
-        $this->bindings[$id] = [
-            'shared' => false,
-            'callback' => $callback,
-        ];
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function once(string $id, Closure $callback)
-    {
-        $this->bindings[$id] = [
-            'shared' => true,
-            'callback' => $callback,
-        ];
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function set(string $id, $value)
-    {
-        $this->instances[$id] = $value;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function make(string $id)
-    {
-        if (array_key_exists($id, $this->instances)) {
-            return $this->instances[$id];
-        }
-
-        if (array_key_exists($id, $this->bindings)) {
-            ['shared' => $shared, 'callback' => $callback] = $this->bindings[$id];
-
-            if ($shared) {
-                return $this->instances[$id] = $callback($this);
-            }
-
-            return $callback($this);
-        }
-
-        return null;
     }
 }
